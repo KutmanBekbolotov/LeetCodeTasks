83b2263a2ed73690e99f4847f22c09d88003999c
kutman <kutmanbekbolotov2003@gmail.com>
2024-11-30 20:24:15 +0600

    added new file

---
+++ python/add.py
@@ -0,0 +1,7 @@
+password = str(input("Enter password: "))
+confiramtion = str(input("Confirm password: "))
+
+if password == confiramtion:
+    print("Password confirmed")
+else:
+    print("Password not confirmed")
